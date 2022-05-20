e9d2ce60538b6cfe6af40045d5ac85b64323994d
Rhutik Giradkar <46106467+rhutikcodes@users.noreply.github.com>
2022-05-20 19:53:35 +0530

    TASK: backend integration

--- frontend/lib/provider/training_provider.dart
+++ frontend/lib/provider/training_provider.dart
@@ -1,5 +1,6 @@
 import 'dart:math';
 
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:logger/logger.dart';
 
@@ -10,43 +11,52 @@ class TrainingProvider with ChangeNotifier {
     Model.SUPPORT_VECTOR_MACHINE: {
       "name": Model.SUPPORT_VECTOR_MACHINE,
       "accuracy": 0,
-      "status": "initial"
+      "status": "initial",
+      "route": "supportVectorMachine"
     },
     Model.ARTIFICIAL_NEURAL_NETWORK: {
       "name": Model.ARTIFICIAL_NEURAL_NETWORK,
       "accuracy": 0,
-      "status": "initial"
+      "status": "initial",
+      "route": "artificialNeuralNetwork"
     },
     Model.NAIVE_BAYES: {
       "name": Model.NAIVE_BAYES,
       "accuracy": 0,
-      "status": "initial"
+      "status": "initial",
+      "route": "naiveBayes"
     },
     Model.LOGISTIC_REGRESSION: {
       "name": Model.LOGISTIC_REGRESSION,
       "accuracy": 0,
-      "status": "initial"
+      "status": "initial",
+      "route": "logisticRegression"
     },
     Model.RANDOM_FORREST: {
       "name": Model.RANDOM_FORREST,
       "accuracy": 0,
-      "status": "initial"
+      "status": "initial",
+      "route": "randomForest"
     }
   };
   Future<void> startTraining(String modelName) async {
+    const BACKEND_API =
+        String.fromEnvironment('BACKEND_API', defaultValue: 'BACKEND_API');
+    print(BACKEND_API);
     for (var element in modelData.entries) {
       if (element.key == modelName) {
         element.value['status'] = 'training';
+        final String route = element.value['route'];
         Logger().d(element);
+        Logger().d(BACKEND_API + route);
         notifyListeners();
-        Future.delayed(const Duration(seconds: 5)).then((value) {
-          var randomValue = Random().nextInt(100);
-          Logger().i(randomValue);
-          element.value['accuracy'] = randomValue;
-          element.value['status'] = 'trained';
-          Logger().e(element);
-          notifyListeners();
-        });
+        Dio().get(BACKEND_API + route).then((value) => {
+              Logger().i(value),
+              element.value['accuracy'] = value.data['accuracy'],
+              element.value['status'] = 'trained',
+              Logger().e(element),
+              notifyListeners(),
+            });
       }
     }
   }
