ebaca165a67a44c5a5b9b1324740381ad9066352
Rhutik Giradkar <46106467+rhutikcodes@users.noreply.github.com>
2022-05-22 14:51:23 +0530

    Update bar_data.dart

--- frontend/lib/repository/bar_data.dart
+++ frontend/lib/repository/bar_data.dart
@@ -10,31 +10,31 @@ class BarData {
     const Data(
       id: 0,
       name: Model.SUPPORT_VECTOR_MACHINE,
-      y: 84.23,
+      y: 97.62,
       color: Color(0xff19bfff),
     ),
     const Data(
       name: Model.ARTIFICIAL_NEURAL_NETWORK,
       id: 1,
-      y: 85.11,
+      y: 99.74,
       color: Color(0xffff4d94),
     ),
     const Data(
       name: Model.NAIVE_BAYES,
       id: 2,
-      y: 80.43,
+      y: 87.83,
       color: Color(0xff2bdb90),
     ),
     const Data(
       name: Model.LOGISTIC_REGRESSION,
       id: 3,
-      y: 84.48,
+      y: 96.56,
       color: Color(0xffffdd80),
     ),
     const Data(
       name: Model.RANDOM_FORREST,
       id: 4,
-      y: 85.74,
+      y: 99.94,
       color: Color.fromARGB(255, 128, 156, 255),
     ),
   ];
